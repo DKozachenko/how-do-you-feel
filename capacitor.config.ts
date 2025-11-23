ed4be62ea115d0486ff2892f6517614862ab2394
Diana Kozachenko <kozachenkodiana03@mail.ru>
2025-11-23 21:43:25 +0100

    Set up Ionic+Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'How Do You Feel',
+  webDir: 'dist/how-do-feel/browser'
+};
+
+export default config;
