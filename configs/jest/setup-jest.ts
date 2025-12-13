d84bce64e97ae3f2722892b1fa6745a379a1034d
Diana Kozachenko <kozachenkodiana03@mail.ru>
2025-12-13 20:56:54 +0100

    chore: add zoneless testing

--- configs/jest/setup-jest.ts
+++ configs/jest/setup-jest.ts
@@ -1,4 +1,3 @@
 /* eslint-disable boundaries/no-unknown-files */
-import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone/index.mjs';
-
-setupZoneTestEnv();
+import { setupZonelessTestEnv } from 'jest-preset-angular/setup-env/zoneless/index.mjs';
+setupZonelessTestEnv();
