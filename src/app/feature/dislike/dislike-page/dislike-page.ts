1f6d495cc994a8bc9206e9bd07775d9d19c6aa18
Diana Kozachenko <kozachenkodiana03@mail.ru>
2026-04-10 22:53:50 +0200

    feat(feature): changing order of action on dislike page

--- src/app/feature/dislike/dislike-page/dislike-page.ts
+++ src/app/feature/dislike/dislike-page/dislike-page.ts
@@ -12,9 +12,13 @@ import {
 } from '@ionic/angular/standalone';
 import { forkJoin, from, of, switchMap } from 'rxjs';
 import { DislikesStorageService } from '@core/dislikes/dislikes-storage.service';
+import { ActionOrder } from '@core/model/action-order.constants';
 import { Action } from '@core/model/action.interface';
 import { ModalRole } from '@core/model/modal-role.enum';
+import { SettingsIds } from '@core/model/setting.interface';
 import { FilterActionsPipe } from '@core/pipes/filter-actions/filter-actions.pipe';
+import { SortActionsPipe } from '@core/pipes/sort-actions/sort-actions.pipe';
+import { SettingsStorageService } from '@core/settings/settings.service';
 import { ActionCard } from '@pattern/action/action-card/action-card';
 import { EditActionModal } from '@pattern/action/edit-action-modal/edit-action-modal';
 import { SearchComponent } from '@ui/search/search';
@@ -34,16 +38,20 @@ import { SearchComponent } from '@ui/search/search';
     ActionCard,
     SearchComponent,
     FilterActionsPipe,
+    SortActionsPipe,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LikePage implements ViewWillEnter {
   private readonly dislikesStorageService = inject(DislikesStorageService);
+  private readonly settingStorageService = inject(SettingsStorageService);
   private readonly modalController = inject(ModalController);
 
   dislikes = signal<Action[]>([]);
   dislikesCount = computed(() => this.dislikes().length);
   searchInput = signal('');
+
+  actionOrder = signal<ActionOrder>(ActionOrder.DEFAULT_ORDER);
 
   ionViewWillEnter(): void {
     this.load();
@@ -79,6 +87,12 @@ export class LikePage implements ViewWillEnter {
   }
 
   private load(): void {
-    this.dislikesStorageService.getAll().subscribe((data) => this.dislikes.set(data ?? []));
+    forkJoin([
+      this.dislikesStorageService.getAll(),
+      this.settingStorageService.getById<ActionOrder>(SettingsIds.ACTION_ORDER),
+    ]).subscribe(([dislikes, setting]) => {
+      this.dislikes.set(dislikes ?? []);
+      this.actionOrder.set(setting?.value ?? ActionOrder.DEFAULT_ORDER);
+    });
   }
 }
